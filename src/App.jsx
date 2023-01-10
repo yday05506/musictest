17346b19990b78ddff03f1345c0e01dddc6ff30c
yday05506 <yangdayeon2@gmail.com>
2023-01-11 00:39:49 +0900

    제목과... 이미지만 넣기... (개힘듦. 40분 소요. 할 수 있다.)

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+      <div className="App">
+        <h1>What's your music?</h1>
+      </div>
+      <div className="start_btn">
+          <img src="img/start_btn.png" alt="start"/>
+      </div>
+      </>
+    );
+  }
+}
+
+export default App;
